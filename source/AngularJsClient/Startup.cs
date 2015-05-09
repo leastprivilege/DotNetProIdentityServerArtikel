25187dd2f5355fa316403ea8ec2528cb45521f5e
Christian Liebel <christian@liebel.org>
2015-05-09 10:46:23 +0200

    Added new project for AngularJS client

---
+++ source/AngularJsClient/Startup.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Owin;
+using Owin;
+using Microsoft.Owin.FileSystems;
+using Microsoft.Owin.StaticFiles;
+
+[assembly: OwinStartup(typeof(AngularJsClient.Startup))]
+
+namespace AngularJsClient
+{
+	public class Startup
+	{
+		public void Configuration(IAppBuilder app)
+		{
+			var physicalFileSystem = new PhysicalFileSystem(@".\web");
+			var options = new FileServerOptions
+			{
+				EnableDefaultFiles = true,
+				FileSystem = physicalFileSystem
+			};
+
+			options.StaticFileOptions.FileSystem = physicalFileSystem;
+			options.StaticFileOptions.ServeUnknownFileTypes = true;
+			options.DefaultFilesOptions.DefaultFileNames = new[] { "index.html" };
+
+			app.UseFileServer(options);
+		}
+	}
+}
