9de719a960bff4c88ad941b8649d9449f65b0ee4
Christian Liebel <christian@liebel.org>
2015-05-10 13:52:59 +0200

    Added routing

---
+++ source/AngularJsClient/web/app/routing.js
@@ -0,0 +1,30 @@
+﻿(function ($, jQuery) {
+    "use strict";
+
+    /**
+     * @param $stateProvider
+     * @param $urlRouterProvider
+     * @constructor
+     */
+    function RoutingConfig($stateProvider, $urlRouterProvider) {
+        $stateProvider
+            .state('main', getState('main', '/'))
+            .state('login', getState('login'))
+            .state('apiAccess', getState('apiAccess'));
+
+        $urlRouterProvider.otherwise('/');
+
+        function getState(key, urlOverride) {
+            var url = urlOverride ? urlOverride : '/' + key;
+
+            return {
+                url: url,
+                templateUrl: 'app/' + key + '/' + key + '.html',
+                controller: key + "Controller"
+            };
+        }
+    }
+
+    app.module.config(RoutingConfig);
+
+})();
