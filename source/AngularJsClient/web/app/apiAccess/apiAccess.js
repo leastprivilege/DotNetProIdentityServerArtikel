45f05a9c5930043045d659d6635a557846de7e44
Christian Liebel <christian@liebel.org>
2015-05-14 03:35:32 +0200

    Implemented API access page

--- source/AngularJsClient/web/app/apiAccess/apiAccess.js
+++ source/AngularJsClient/web/app/apiAccess/apiAccess.js
@@ -7,10 +7,15 @@
      *
      * @param $rootScope
      * @param $scope
+     * @param {TestApi} testApi
      */
-    function ApiAccessController($rootScope, $scope) {
+    function ApiAccessController($rootScope, $scope, testApi) {
         $rootScope.pageTitle = "API Access";
 
+        testApi.getTestAsync()
+            .then(function (result) {
+                $scope.result = result;
+            });
     }
 
     app.module.controller('apiAccessController', ApiAccessController);
