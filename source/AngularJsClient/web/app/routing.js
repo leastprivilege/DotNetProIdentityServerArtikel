ea149a62b6a6aa561b47b53ddbb7b4895b879ef3
Christian Liebel <christian@liebel.org>
2015-05-14 03:36:30 +0200

    Implemented anonymous states

--- source/AngularJsClient/web/app/routing.js
+++ source/AngularJsClient/web/app/routing.js
@@ -4,23 +4,25 @@
     /**
      * @param $stateProvider
      * @param $urlRouterProvider
+     *
      * @constructor
      */
     function RoutingConfig($stateProvider, $urlRouterProvider) {
         $stateProvider
-            .state('main', getState('main', '/'))
+            .state('main', getState('main', '/', false))
             .state('login', getState('login'))
             .state('apiAccess', getState('apiAccess'));
 
         $urlRouterProvider.otherwise('/');
 
-        function getState(key, urlOverride) {
+        function getState(key, urlOverride, anonymous) {
             var url = urlOverride ? urlOverride : '/' + key;
 
             return {
                 url: url,
                 templateUrl: 'app/' + key + '/' + key + '.html',
-                controller: key + "Controller"
+                controller: key + "Controller",
+                anonymous: !!anonymous
             };
         }
     }
